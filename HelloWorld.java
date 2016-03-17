fcf01cc45312911c1b6ceae954e87cf1d6723ab7
aprilpaoyin <aprilpaoyin@gmail.com>
2016-03-17 22:08:53 +0000

    Added files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * Class that prints 'Hello World'
+ * Author: April Tan Pao Yin
+ * Date: 31 Jan 2016
+ */
+
+package com.examples.test;
+
+public class HelloWorld 
+{
+	public static void main(String args[])
+	{
+		String phrase = "Eat dicks";
+		System.out.println(phrase);
+	}//end main
+}
